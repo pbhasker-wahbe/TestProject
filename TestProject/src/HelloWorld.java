d94ffdacf3152c65597bf87e85b61ad1ac6b83aa
prbhasker <prbhasker@USMECPRBHASKER2.us.deloitte.com>
2020-07-23 16:45:53 -0400

    adding another method

--- TestProject/src/HelloWorld.java
+++ TestProject/src/HelloWorld.java
@@ -6,5 +6,12 @@ public class HelloWorld {
 	}
 	public static void main(String args[]) {
 		System.out.println("First GitHub project");
+		int k =getSum(1,5);
+		
+		System.out.println("sum= "+k);
+	}
+	
+	public static int getSum(int x, int y) {
+		return x+y;
 	}
 }
